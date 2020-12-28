60815a6c17594fecd406cc71d646db3d2895abbc
Muhammad Wyzer <wyzer@Wyzer-MacBook-Pro.local>
2020-12-28 16:56:46 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const dice = {
+  1: require("./assets/dice_1.png"),
+  2: require("./assets/dice_2.png"),
+  3: require("./assets/dice_3.png"),
+  4: require("./assets/dice_4.png"),
+  5: require("./assets/dice_5.png"),
+  6: require("./assets/dice_6.png")
+
+}
 
 export default function App() {
+  
+  const [dice1, setDice1] = useState("1") 
+  const [dice2, setDice2] = useState("5")
+
+  function rollTheDice() {
+    const dice1Random = (Math.floor(Math.random() * 6 + 1)).toString();
+    const dice2Random = (Math.floor(Math.random() * 6 + 1)).toString();
+    
+    setDice1(dice1Random)
+    setDice2(dice2Random)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View
+        style={{
+          flexDirection: "row"
+        }}
+      >
+        <Image
+          source={dice[dice1]}
+          style={{
+            width: 128,
+            height: 128
+          }}
+
+        />
+
+        <Image
+          source={dice[dice2]}
+          style={{
+            width: 128,
+            height: 128,
+            marginStart: 20
+          }}
+
+        />
+
+      </View>
+
+      <TouchableOpacity 
+        onPress={rollTheDice}
+      >
+        <View
+          style={{
+            backgroundColor: "black",
+            marginTop: 40,
+            paddingHorizontal: 40,
+            paddingVertical: 20,
+            borderRadius: 8
+          }}
+        >
+          <Text
+            style={{
+              color: "#fff"
+            }}
+          >ROLL THE DICE</Text>
+
+        </View>
+
+      </TouchableOpacity>
+       
     </View>
   );
 }
